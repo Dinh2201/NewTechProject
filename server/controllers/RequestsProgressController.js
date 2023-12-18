95b916c7fff6a2b2d251c97762fdf38491bb5a2f
Hưng Võ <80050392+huwng12@users.noreply.github.com>
2023-12-18 15:07:25 +0700

    update fixbug

--- server/controllers/RequestsProgressController.js
+++ server/controllers/RequestsProgressController.js
@@ -1,107 +1,197 @@
-const ProjectRegistrationPeriod = require('../models/ProjectRegistrationPeriodModel');
+const RequestsProgress = require('../models/RequestsProgressModel');
+const Document = require('../models/DocumentModel');
+const multer = require('multer');
+const fs = require('fs');
+const path = require('path');
 
-// Thêm mới đợt đăng kí đề tài
-exports.addProjectRegistrationPeriod = async (req, res) => {
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'public/file/document');
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname);
+    },
+});
+
+const upload = multer({ storage: storage });
+
+// Lấy tất cả yêu cầu
+exports.getRequestsProgresssByThesisId = async (req, res) => {
     try {
-        const { YearID, Title, StartDate, EndDate } = req.body;
+        const requestsProgressList = await RequestsProgress
+            .find({ ThesisID: req.params.thesesId })
+            .exec();
 
-        const existingProjectRegistrationPeriod = await ProjectRegistrationPeriod.findOne({ Title: Title });
+        if (requestsProgressList.length === 0) {
+            return res.status(404).json({ message: 'Không có RequestsProgress nào tương ứng với ThesisID' });
+        }
 
-        if (existingProjectRegistrationPeriod) {
-            res.status(401).json({ message: 'Đợt đăng kí đã tồn tại!' });
-        } else {
+        const result = [];
 
-            const newProjectRegistrationPeriod = new ProjectRegistrationPeriod({
-                YearID,
-                Title,
-                StartDate,
-                EndDate
+        // Lặp qua danh sách các RequestsProgress
+        for (const requestsProgress of requestsProgressList) {
+            const documents = await Document
+                .find({ RequestID: requestsProgress._id })
+                .exec();
+
+            // Thêm thông tin về Documents vào mỗi RequestsProgress
+            const requestsProgressWithDocuments = { ...requestsProgress.toObject(), Documents: documents };
+            result.push(requestsProgressWithDocuments);
+        }
+
+        res.json(result);
+    } catch (error) {
+        res.status(500).json({ message: 'Lỗi khi lấy yêu cầu' });
+    }
+};
+
+// Thêm mới yêu cầu
+exports.addRequestsProgress = async (req, res) => {
+    try {
+        upload.array('files', 5)(req, res, async (err) => {
+            if (err) {
+                // Xử lý lỗi nếu có
+                console.error('Lỗi' + err);
+                return res.status(500).json({ message: 'Lỗi khi xử lý tệp tải lên' + err });
+            }
+
+            const { ThesisID, AdvisorID, RequestDescription } = req.body;
+
+            const newRequestsProgress = new RequestsProgress({
+                ThesisID,
+                AdvisorID,
+                RequestDescription,
+                Progress: 0,
             });
 
-            await newProjectRegistrationPeriod.save();
+            await newRequestsProgress.save();
 
-            res.status(200).json({ message: 'Đợt đăng kí đã được thêm thành công!' });
-        }
-    } catch (error) {
-        res.status(500).json({ error: 'Lỗi khi thêm đợt đăng kí: ' + error.message });
-    }
-};
-// Lấy tất cả đợt đăng kí
-exports.getAllProjectRegistrationPeriods = async (req, res) => {
-    try {
-        // Lấy giá trị của tham số department từ request
-        const { academicyear } = req.params;
-
-        // Tạo một đối tượng chứa các điều kiện tìm kiếm
-        const conditions = {};
-        if (academicyear) {
-            if (academicyear !== 'All') {
-                conditions.YearID = academicyear;
+            if (req.files && req.files.length > 0) {
+                const fileUrls = req.files.map((file) => file.filename);
+                for (const fileUrl of fileUrls) {
+                    const document = new Document({
+                        RequestID: newRequestsProgress._id,
+                        DocumentPath: `/file/document/${fileUrl}`,
+                        Type: 'request'
+                    });
+                    await document.save();
+                }
             }
-        }
-        const students = await ProjectRegistrationPeriod.find(conditions).populate('YearID');
-        res.json(students);
+
+            res.status(200).json({ message: 'Yêu cầu đã được thêm thành công!' });
+        })
     } catch (error) {
-        res.status(500).json({ message: 'Lỗi khi lấy danh sách đợt đăng kí' });
+        res.status(500).json({ error: 'Lỗi khi thêm yêu cầu: ' + error.message });
     }
 };
 
-// Lấy thông tin đợt đăng kí bằng ID
-exports.getProjectRegistrationPeriodById = async (req, res) => {
+// Lấy thông tin yêu cầu bằng ID
+exports.getRequestsProgressById = async (req, res) => {
     try {
-        const projectRegistrationPeriod = await ProjectRegistrationPeriod.findById(req.params.projectRegistrationPeriodId).populate('YearID');
-        if (!projectRegistrationPeriod) {
-            return res.status(404).json({ message: 'Đợt đăng kí không tồn tại' });
+        const requestsProgress = await RequestsProgress.findById(req.params.requestsProgressId);
+
+        if (!requestsProgress) {
+            return res.status(404).json({ message: 'Yêu cầu không tồn tại' });
         }
+
+        const documents = await Document
+            .find({ RequestID: requestsProgress._id, Type: req.params.type });
+
         res.json({
-            projectRegistrationPeriod: projectRegistrationPeriod,
+            requestsProgress: requestsProgress,
+            documents: documents
         });
     } catch (error) {
-        res.status(500).json({ message: 'Lỗi khi lấy thông tin đợt đăng kí' });
+        res.status(500).json({ message: 'Lỗi khi lấy thông tin yêu cầu' });
     }
 };
 
-// Cập nhật thông tin đợt đăng kí bằng ID
-exports.editProjectRegistrationPeriod = async (req, res) => {
+// Cập nhật thông tin yêu cầu bằng ID
+exports.editRequestsProgress = async (req, res) => {
     try {
-        const { projectRegistrationPeriodId } = req.params;
-        const { YearID, Title, StartDate, EndDate } = req.body;
+        upload.array('files', 5)(req, res, async (err) => {
+            if (err) {
+                // Xử lý lỗi nếu có
+                console.error('Lỗi' + err);
+                return res.status(500).json({ message: 'Lỗi khi xử lý tệp tải lên' + err });
+            }
+            const { ThesisID, AdvisorID, RequestDescription, Progress, Type } = req.body;
 
-        // Kiểm tra xem sinh viên có tồn tại không
-        const existingProjectRegistrationPeriod = await ProjectRegistrationPeriod.findById(projectRegistrationPeriodId).populate('YearID');
+            // Kiểm tra xem yêu cầu có tồn tại không
+            const requestsProgress = await RequestsProgress.findById(req.params.requestsProgressId);
 
-        if (!existingProjectRegistrationPeriod) {
-            res.status(404).json({ message: 'Đợt đăng kí không tồn tại!' });
-            return;
-        }
+            if (!requestsProgress) {
+                res.status(404).json({ message: 'Yêu cầu không tồn tại!' });
+                return;
+            }
 
-        // Cập nhật thông tin sinh viên
-        existingProjectRegistrationPeriod.YearID = YearID;
-        existingProjectRegistrationPeriod.Title = Title;
-        existingProjectRegistrationPeriod.StartDate = StartDate;
-        existingProjectRegistrationPeriod.EndDate = EndDate;
+            // Cập nhật thông tin yêu cầu
+            if (ThesisID && AdvisorID && RequestDescription) {
+                requestsProgress.ThesisID = ThesisID;
+                requestsProgress.AdvisorID = AdvisorID;
+                requestsProgress.RequestDescription = RequestDescription;
+            }
+            if (Progress) {
+                requestsProgress.Progress = Progress;
+            }
 
-        // Lưu các thay đổi
-        await existingProjectRegistrationPeriod.save();
+            // Lưu các thay đổi
+            await requestsProgress.save();
+            var type = 'request';
+            if (Type) {
+                type = Type;
+            }
+            if (req.files && req.files.length > 0) {
+                const fileUrls = req.files.map((file) => file.filename);
+                for (const fileUrl of fileUrls) {
 
-        res.status(200).json({ message: 'Thông tin đợt đăng đã được cập nhật thành công!' });
+                    const document = new Document({
+                        RequestID: requestsProgress._id,
+                        DocumentPath: `/file/document/${fileUrl}`,
+                        Type: type
+                    });
+                    await document.save();
+                }
+            }
+
+            res.status(200).json({ message: 'Thông tin yêu cầu đã được cập nhật thành công!' });
+        })
     } catch (error) {
-        res.status(500).json({ error: 'Lỗi khi cập nhật thông tin đợt đăng kí: ' + error.message });
+        res.status(500).json({ error: 'Lỗi khi cập nhật thông tin yêu cầu: ' + error.message });
     }
 };
 
-// Xóa đợt đăng kí bằng ID
-exports.deleteProjectRegistrationPeriod = async (req, res) => {
+// Xóa yêu cầu bằng ID
+exports.deleteRequestsProgress = async (req, res) => {
     try {
-        const projectRegistrationPeriodId = await ProjectRegistrationPeriod.findById(req.params.projectRegistrationPeriodId);
-        if (!projectRegistrationPeriodId) {
-            return res.status(404).json({ message: 'Đợt đăng kí không tồn tại' });
+        const requestsProgressId = req.params.requestsProgressId;
+        const requestsProgress = await RequestsProgress.findById(requestsProgressId);
+        if (!requestsProgress) {
+            return res.status(404).json({ message: 'Yêu cầu không tồn tại' });
         }
+        await Document.deleteMany({ RequestID: requestsProgressId });
 
-        await ProjectRegistrationPeriod.findByIdAndDelete(req.params.projectRegistrationPeriodId);
+        await RequestsProgress.findByIdAndDelete(requestsProgressId);
 
-        res.json({ message: 'Đợt đăng kí đã bị xóa' });
+        res.json({ message: 'Yêu cầu đã bị xóa' });
     } catch (error) {
-        res.status(500).json({ message: 'Lỗi khi xóa đợt đăng kí' });
+        res.status(500).json({ message: 'Lỗi khi xóa yêu cầu' });
+    }
+};
+
+exports.deleteDocument = async (req, res) => {
+    try {
+        const document = await Document.findById(req.params.documentId);
+        if (!document) {
+            return res.status(404).json({ message: 'Tài liệu không tồn tại' });
+        }
+        const filePath = path.join(__dirname, '..', 'public', document.DocumentPath);
+        fs.unlinkSync(filePath);
+
+        await Document.findByIdAndDelete(req.params.documentId);
+
+        res.json({ message: 'Tài liệu đã bị xóa' });
+    } catch (error) {
+        res.status(500).json({ message: 'Lỗi khi xóa tài liệu' });
     }
 };
--- server/migrations/1700034765712-add_initial_data.js
+++ server/migrations/1700034765712-add_initial_data.js
@@ -3,7 +3,7 @@ const bcrypt = require('bcrypt');
 
 const connectDB = async () => {
   try {
-    const connection = await mongoose.connect('mongodb://0.0.0.0:27017/MEVNDatabase', {
+    const connection = await mongoose.connect('mongodb://0.0.0.0:27017/MERNDatabase', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
@@ -22,18 +22,40 @@ module.exports = {
     const usersCollection = database.collection("users");
     const academicyearsCollection = database.collection("academicyears");
     const departmentsCollection = database.collection("departments");
+    const teacherCollection = database.collection("teachers");
 
     const saltRounds = 10;
     const hashedPassword = await bcrypt.hash('12345678', saltRounds);
 
-    await usersCollection.insertOne({
-      UserName: 'admin',
-      Password: hashedPassword,
-    });
+    const usersData = [
+      {
+        UserName: 'admin',
+        Password: hashedPassword,
+        Role: 'admin'
+      },
+      {
+        UserName: 'teacher',
+        Password: hashedPassword,
+        Role: 'teacher'
+      },
+      {
+        UserName: 'departmentHead',
+        Password: hashedPassword,
+        Role: 'departmentHead'
+      }
+    ];
+
+    await usersCollection.insertMany(usersData);
 
     await academicyearsCollection.insertMany([
       {
         YearName: '2018-2022'
+      },
+      {
+        YearName: '2019-2023'
+      },
+      {
+        YearName: '2020-2024'
       }
     ]);
 
@@ -41,8 +63,56 @@ module.exports = {
       {
         DepartmentName: "Tin học ứng dụng",
         Description: 'Tin học ứng dụng'
+      },
+      {
+        DepartmentName: "Công nghệ thông tin",
+        Description: "Công nghệ thông tin"
+      },
+      {
+        DepartmentName: "Khoa học máy tính",
+        Description: "Khoa học máy tính"
       }
     ]);
+
+    const department = await departmentsCollection.findOne({ DepartmentName: "Tin học ứng dụng" });
+    const admin = await usersCollection.findOne({ Role: "admin" });
+    const teacher = await usersCollection.findOne({ Role: "teacher" });
+    const departmentHead = await usersCollection.findOne({ Role: "departmentHead" });
+
+    const teachersData = [
+      {
+        UserID: admin._id,
+        DepartmentID: department._id,
+        FullName: 'Nguyen Admin',
+        DateOfBirth: new Date('1990-01-01'),
+        Email: 'teacher1@example.com',
+        Phone: '123456789',
+        Address: '123 Main Street',
+        Position: "Quản trị viên",
+      },
+      {
+        UserID: teacher._id,
+        DepartmentID: department._id,
+        FullName: 'Tran Teacher',
+        DateOfBirth: new Date('1985-05-15'),
+        Email: 'teacher2@example.com',
+        Phone: '987654321',
+        Address: '456 Second Street',
+        Position: "Giảng viên",
+      },
+      {
+        UserID: departmentHead._id,
+        DepartmentID: department._id,
+        FullName: "Le DepartmentHead",
+        DateOfBirth: new Date('1985-05-15'),
+        Email: 'teacher2@example.com',
+        Phone: '987654321',
+        Address: '456 Second Street',
+        Position: "Trưởng bộ môn",
+      },
+    ];
+
+    await teacherCollection.insertMany(teachersData);
 
   },
 
@@ -52,12 +122,14 @@ module.exports = {
     const usersCollection = database.collection("users");
     const academicyearsCollection = database.collection("academicyears");
     const departmentsCollection = database.collection("departments");
+    const teacherCollection = database.collection("teachers");
 
     await departmentsCollection.deleteMany({});
 
     await usersCollection.deleteMany({});
 
     await academicyearsCollection.deleteMany({});
+    await teacherCollection.deleteMany({});
 
   }
 };
